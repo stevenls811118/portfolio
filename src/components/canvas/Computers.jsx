0f70d46d04393f46d39840e66dff2cac02ed114e
Yifan Shi <stevenls1118@msn.com>
2023-08-28 23:58:57 -0600

    update hiding computer from screen when screen height less than 650px

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable react/no-unknown-property */
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas, events } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
+const Computers = ({ isMobile, isShortScreen }) => {
+
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.45} groundColor="black"/>
+      <pointLight intensity={1.2} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      {!isShortScreen && 
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.5 : 0.75}
+          position={[0, -3.5, -1.5]}
+          rotation={[0, 0.25, -0.05]}
+        />
+      }
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isShortScreen, setIsShortScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-height: 650px)');
+
+    setIsShortScreen(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsShortScreen(e.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
+  return (
+    <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} isShortScreen={isShortScreen} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+export default ComputersCanvas;
